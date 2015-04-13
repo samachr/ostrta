4e4fd452aef8c23a8c20fcb954c08ee4bb69f4bd
Samuel Christensen <samachr@gmail.com>
2015-04-13 09:47:04 -0600

    cleaned working directory

--- serverGlue.sh
+++
@@ -1,7 +0,0 @@
-#node shell.js  < fifo | ./searchEngine/a.out > fifo
-
-mkfifo fifo0 fifo1
-( ./a.out > fifo0 < fifo1 ) &
-( node server.js > fifo1 < fifo0 ) &
-( exec 30<fifo0 31<fifo1 )
-rm fifo0 fifo1
